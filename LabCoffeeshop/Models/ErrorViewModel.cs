6f36b001e963881cb99c544cda6f4203b5383c3e
belle <belle.baxley@gmail.com>
2023-02-10 08:56:43 -0500

    first commit

---
+++ LabCoffeeshop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LabCoffeeshop.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
